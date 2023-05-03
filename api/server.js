a6b43f36711cbf8128d6f1d42b08823e02bab58e
Kabeer Shahzad <kabeer.shahzad@outlook.com>
2023-05-03 16:34:21 +0500

    express api

--- api/server.js
+++ api/server.js
@@ -1,6 +1,13 @@
 import express from "express";
 import mongoose from "mongoose";
 import dotenv from "dotenv";
+import userRouter from "./routes/user.route.js";
+import gigRouter from "./routes/gig.route.js";
+import orderRouter from "./routes/order.route.js";
+import conversationRouter from "./routes/conversation.route.js";
+import messageRouter from "./routes/message.route.js";
+import reviewRouter from "./routes/review.route.js";
+import authRoute from "./routes/auth.route.js";
 
 const app = express();
 dotenv.config();
@@ -15,6 +22,15 @@ const connect = async () => {
     handleError(error);
   }
 };
+app.use("/api/auth", authRoute);
+
+app.use("/api/users", userRouter);
+// app.use("/api/gigs", gigRoute);
+// app.use("/api/orders", orderRoute);
+// app.use("/api/conversations", conversationRoute);
+// app.use("/api/messages", messageRoute);
+// app.use("/api/reviews", reviewRoute);
+
 app.listen(8800, () => {
   connect();
   console.log("Back is running on");
