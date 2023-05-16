e2dc8bb9bb483357a26bd1168d295eb51863e471
Kabeer Shahzad <kabeer.shahzad@outlook.com>
2023-05-16 13:53:31 +0500

    Mongo_err_con_res

--- api/server.js
+++ api/server.js
@@ -1,47 +1,40 @@
 import express from "express";
 import mongoose from "mongoose";
 import dotenv from "dotenv";
-import userRouter from "./routes/user.route.js";
-import gigRouter from "./routes/gig.route.js";
-import orderRouter from "./routes/order.route.js";
-import conversationRouter from "./routes/conversation.route.js";
-import messageRouter from "./routes/message.route.js";
-import reviewRouter from "./routes/review.route.js";
+import userRoute from "./routes/user.route.js";
+import gigRoute from "./routes/gig.route.js";
+import orderRoute from "./routes/order.route.js";
+import conversationRoute from "./routes/conversation.route.js";
+import messageRoute from "./routes/message.route.js";
+import reviewRoute from "./routes/review.route.js";
 import authRoute from "./routes/auth.route.js";
 import cookieParser from "cookie-parser";
 import cors from "cors";
 
 const app = express();
 dotenv.config();
-//mongoose.set("StrictQuery", true);
+mongoose.set("strictQuery", true);
 
-//{mongodb connection
 const connect = async () => {
   try {
     await mongoose.connect(process.env.MONGO);
-    console.log("Mongodb is connected successfully.");
+    console.log("Connected to mongoDB!");
   } catch (error) {
     console.log(error);
   }
 };
 
-app.use(
-  cors({
-    origin: "http://localhost:5173",
-
-    credentials: true,
-  })
-);
+app.use(cors({ origin: "http://localhost:5173", credentials: true }));
 app.use(express.json());
 app.use(cookieParser());
 
 app.use("/api/auth", authRoute);
-app.use("/api/users", userRouter);
-app.use("/api/gigs", gigRouter);
-app.use("/api/orders", orderRouter);
-app.use("/api/conversations", conversationRouter);
-app.use("/api/messages", messageRouter);
-app.use("/api/reviews", reviewRouter);
+app.use("/api/users", userRoute);
+app.use("/api/gigs", gigRoute);
+app.use("/api/orders", orderRoute);
+app.use("/api/conversations", conversationRoute);
+app.use("/api/messages", messageRoute);
+app.use("/api/reviews", reviewRoute);
 
 app.use((err, req, res, next) => {
   const errorStatus = err.status || 500;
@@ -49,7 +42,8 @@ app.use((err, req, res, next) => {
 
   return res.status(errorStatus).send(errorMessage);
 });
+
 app.listen(8800, () => {
   connect();
-  console.log("Back is running on");
+  console.log("Backend server is running!");
 });
