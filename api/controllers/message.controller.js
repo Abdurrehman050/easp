b0bd27d30a00feedbe75145155d1ba5a1528fdfc
Kabeer Shahzad <kabeer.shahzad@outlook.com>
2023-05-23 23:42:16 +0500

    Fetch Orders and Conversation

---
+++ api/controllers/message.controller.js
@@ -0,0 +1,8 @@
+import createError from "../utils/createError";
+
+export const createOrder = async (req, res, next) => {
+  try {
+  } catch (err) {
+    next(err);
+  }
+};
--- frontend/src/pages/gigs/Gigs.jsx
+++ frontend/src/pages/gigs/Gigs.jsx
@@ -25,7 +25,7 @@ function Gigs() {
         }),
   });
 
-  console.log(data);
+  //console.log(data);
 
   const reSort = (type) => {
     setSort(type);
