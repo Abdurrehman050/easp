bd89b1bde2a2fb79ffb2d0210715307c477ea355
Abdurrehman050 <abdurrehmanfsd07@gmail.com>
2023-05-24 02:45:33 +0500

    message feature error

--- api/routes/message.route.js
+++ api/routes/message.route.js
@@ -1,5 +1,12 @@
 import express from "express";
+import {
+  createMessage,
+  getMessages,
+} from "../controllers/message.controller.js";
+import { verifyToken } from "../middleware/jwt.js";
 
 const router = express.Router();
+router.post("/", verifyToken, createMessage);
+router.post("/:id", verifyToken, getMessages);
 
 export default router;
